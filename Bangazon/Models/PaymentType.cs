78554993d892331cb6e44cbc33799a1f33a6a24c
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-08-04 14:13:37 -0500

    Added customer object on payment type model

---
+++ Bangazon/Models/PaymentType.cs
@@ -0,0 +1,17 @@
+using System;
+
+//Written by: Eliza Meeks
+
+namespace BangazonCLI
+{
+    // Gets and sets public properties of a payment type.
+    public class PaymentType
+    {
+        public int paymentTypeID {get;}
+        public int customerID {get; set;}
+        public int accountNumber {get; set;}
+        public string name {get; set;}
+        public Customer customer {get; set;}
+
+    }
+}
