571370db78027179b3408f629818a689003b7779
Adam Reidelbach <adamreidelbach@gmail.com>
2017-08-04 15:35:32 -0500

    updated product.cs to match Chaz

--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -1,17 +1,17 @@
 using System;
 
-//Written by:  Chaz Henricks
-namespace BangazonCLI
-{
-    // Gets and sets public properties of a Product.
-    public class Product
-    {
-        public int ProductId {get;}
-        public string Name {get; set;}
-        public string Description {get; set;}
-        public int Price {get; set;}
-        public DateTime? DateCreated {get; set;}
-        public Customer customer {get; set;}
-    public ProductType productType {get; set;}
-    }
-} 
+  //Written by:  Chaz Henricks
+  namespace BangazonCLI
+  {
+     // Gets and sets public properties of a Product.
+     public class Product
+     {
+         public int ProductId {get;}
+         public string Name {get; set;}
+         public string Description {get; set;}
+         public Double Price {get; set;}
+         public DateTime? DateCreated {get; set;}
+         public int CustomerID {get; set;}
+         public int ProductTypeID {get; set;}
+     }
+ } 
