5191f4505f80264e43b1ee283082302d24ad1d17
Adam Reidelbach <adamreidelbach@gmail.com>
2017-08-08 14:34:53 -0500

    updated order manager

--- Bangazon/Managers/OrderManager.cs
+++ Bangazon/Managers/OrderManager.cs
@@ -1,25 +1,24 @@
+using System;
 using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
 
-//Written by: Adam Reidelbach
-namespace BangazonCLI
-{
-    // Manages order related methods
-    public class OrderManager
+    //Written by: Adam Reidelbach
+    namespace BangazonCLI
     {
-        private dbUtilities _db;
-        // Constructor method to establish a connection with the database, database conenction is passed in as an argument..
-        public OrderManager(dbUtilities db)
+        // Manages order related methods
+        public class OrderManager
         {
-            _db = db;
-        }
+            private dbUtilities _db;
+            // Constructor method to establish a connection with the database, database conenction is passed in as an argument..
+            public OrderManager(dbUtilities db)
+            {
+                _db = db;
+            }
+            // Adds a product to the active customer's order
+            public int AddProductToOrder(ProductOrder productOrder){
+            int id = _db.Insert( $"insert into productOrder values (null, '{productOrder.productID}', '{productOrder.orderID}')");
 
-        public bool GetCustomerOrders(Order order){
-            
-        }
-
-        // Adds a product to the active customer's order
-        public bool AddProductToOrder(Product product){
-            return true;
+            return id;
         }
     }
 }
