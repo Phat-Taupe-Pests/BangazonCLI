98838b25ade6a4d423ef04196a27a9296e7ff84c
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-08-08 10:56:20 -0500

    Took out colors because it's causing problems and it's not worth it yet

--- Bangazon/MenuActions/CreatePayment.cs
+++ Bangazon/MenuActions/CreatePayment.cs
@@ -10,19 +10,15 @@ namespace BangazonCLI
             Console.Clear();
 
             Console.WriteLine($"Enter payment type (e.g. AmEx, Visa, Checking)");
-            Console.ForegroundColor= ConsoleColor.Red;
+
             Console.WriteLine($">");
 
             string paymentName = Console.ReadLine();
-
-            Console.ForegroundColor= ConsoleColor.Black;
             Console.WriteLine($"Enter Account Number");
-            Console.ForegroundColor= ConsoleColor.Red;
             Console.WriteLine($">");
 
             int accountNumber = int.Parse(Console.ReadLine());
 
-            Console.ForegroundColor= ConsoleColor.Black;
             PaymentType newPaymentType = new PaymentType() { accountNumber=accountNumber, name=paymentName };
             ptm.AddNewPaymentType(newPaymentType);
 
