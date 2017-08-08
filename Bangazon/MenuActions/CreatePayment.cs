f77ce9e2028998e837738f03e651518060dd9d2f
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-08-08 11:04:09 -0500

    Commented CreatePayment.cs

--- Bangazon/MenuActions/CreatePayment.cs
+++ Bangazon/MenuActions/CreatePayment.cs
@@ -2,18 +2,21 @@ using System;
 
 namespace BangazonCLI
 {
+    // Written by: Eliza Meeks
+    // Menu action to create a payment type.
     public class CreatePayment
     {
+        // Method to create the menu and allow the user to input a payment type name and account number.
         public static void CreatePaymentMenu(PaymentTypeManager ptm)
         {
             
             Console.Clear();
-
+            //Prompt and then takes payment type
             Console.WriteLine($"Enter payment type (e.g. AmEx, Visa, Checking)");
-
             Console.WriteLine($">");
-
             string paymentName = Console.ReadLine();
+
+            //Prompts and then takes account number
             Console.WriteLine($"Enter Account Number");
             Console.WriteLine($">");
 
