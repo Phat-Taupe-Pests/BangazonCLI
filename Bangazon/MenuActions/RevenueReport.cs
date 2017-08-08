85cb5602167d5f3063916f0b4a33e14859090897
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-08-08 15:01:31 -0500

    Test for revenue report passes

---
+++ Bangazon/MenuActions/RevenueReport.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace BangazonCLI
+{
+    public class RevenueReport
+    {
+        // Written By : Matt Augsburger
+        public Customer cust { get; set; }
+        public List<Order> orderList { get; set; }
+    
+    }
+}
