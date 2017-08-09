7f1a840e372cd532ac8c7e1812e1a0f1a7d143bf
Matthew Augsburger <matthewaugsburger@gmail.com>
2017-08-09 08:46:29 -0500

    stashing until order ticket is complete

--- Bangazon/MenuActions/RevenueReport.cs
+++ Bangazon/MenuActions/RevenueReport.cs
@@ -6,8 +6,41 @@ namespace BangazonCLI
     public class RevenueReport
     {
         // Written By : Matt Augsburger
-        public Customer cust { get; set; }
         public List<Order> orderList { get; set; }
+
+        public RevenueReport()
+        {
+            // orderList = GetCustomerOrders(CustomerManager.currentCustomer.customerID);
+        }
+
+        public static void ShowRevenueReport()
+        {
+            double total = 0;
+            if (CustomerManager.currentCustomer == null)
+            {
+                return;
+            }
+            Console.Clear();
+            Console.WriteLine($"Revenue Report for {CustomerManager.currentCustomer.firstName} {CustomerManager.currentCustomer.lastName}");
+            Console.WriteLine("");
+            
+            foreach(Order o in orderList)
+            {
+                string dash = "-";
+                Console.WriteLine($"Order #{o.orderID}");
+                Console.WriteLine(dash.PadLeft(24, '-'));
+                foreach(Product p in o.products)
+                {
+                    total += (p.price * p.quantity);
+                    Console.WriteLine($"{p.name}{p.quantity.ToString().PadLeft(15, ' ')}{p.price.ToString().PadLeft(5, ' ')}");
+                    Console.WriteLine("");
+                }
+            }
+            Console.WriteLine($"Total Revenue: ${total}");
+        }
     
     }
+
+
+
 }
