2c809bed99c8f9cb165172530c64649acb9f6dc3
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-08-10 10:19:29 -0500

    SQL query written.

--- Bangazon/Managers/OrderManager.cs
+++ Bangazon/Managers/OrderManager.cs
@@ -54,5 +54,28 @@ using Microsoft.Data.Sqlite;
             _db.Insert( $"insert into productOrder values (null, {productID}, {_orderID})");
             return _orderID;
         }
+
+        // Gets stale products
+        public List<Product> getStaleProducts()
+        {
+            int customerID = CustomerManager.currentCustomer.customerID;
+            List<Product> staleProducts = new List<Product>();
+            _db.Query($"SELECT p.* FROM Product p INNER JOIN ProductOrder po On p.ProductID = po.ProductID INNER JOIN `Order` o ON po.OrderID = o.OrderID AND o.PaymentTypeID IS NOT NULL AND p.Quantity != 0 AND p.DateCreated < date('now','-180 days') UNION SELECT p.* FROM Product p, `Order` o INNER JOIN ProductOrder po ON p.productID = po.productID WHERE p.customerID = 1 AND o.paymentTypeID IS NULL AND o.DateCreated <= date('now', '-90 days') UNION SELECT p.* FROM Product p LEFT JOIN ProductOrder po ON p.ProductID = po.ProductID WHERE po.ProductID IS NULL AND p.DateCreated <= date('now','-180 days') GROUP BY p.Name;", (SqliteDataReader reader) => {
+                while (reader.Read())
+                {
+                    staleProducts.Add(new Product(){
+                        productID = reader.GetInt32(0),
+                        name = reader[1].ToString(),
+                        description = reader[2].ToString(),
+                        price =  reader.GetDouble(3),
+                        dateCreated = reader.GetDateTime(4),
+                        quantity = reader.GetInt32(5),
+                        customerID = reader.GetInt32(6),
+                        productTypeID = reader.GetInt32(7)
+                    });
+                }
+            });
+            return staleProducts;
+        }
     }
 }
